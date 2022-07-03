ab7b09df90fc6f2b43532a4cb9a953ff47f38624
Ebrahim Hosseini <sehosseini@me.com>
2022-07-03 16:28:55 +0430

    remove pod from source

--- Pods/Target Support Files/Pods-KiliaroPhotoGallery-KiliaroPhotoGalleryUITests/Pods-KiliaroPhotoGallery-KiliaroPhotoGalleryUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KiliaroPhotoGallery_KiliaroPhotoGalleryUITests : NSObject
-@end
-@implementation PodsDummy_Pods_KiliaroPhotoGallery_KiliaroPhotoGalleryUITests
-@end
